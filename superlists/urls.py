027da396c121a28362104a50effdb3de046d01d1
James Biederbeck <james@jamesbiederbeck.com>
2018-12-08 18:39:19 -0800

    first unit test and url mapping, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Note: this code doesn't match the book, since in the latest version of django (at least) the django.core.urlresolvers
+# module has been superseded by the django.urls module
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',views.home_page,name='home'),
+    path('admin/', admin.site.urls)
 ]
