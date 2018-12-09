74cdc2407ff23096cec021506711f9709dfb316d
James Biederbeck <james@jamesbiederbeck.com>
2018-12-08 19:35:15 -0800

    Refactored code for Django 1.11 by merging example code from book

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,12 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.func,home_page)
+        self.assertEqual(found.func, home_page)
